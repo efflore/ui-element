2d9bd6a99593f502fe2054f1f48a2ed5e23e7c07
Esther Brunner <esther.brunner@zeix.com>
2024-08-07 18:18:53 +0200

    better build process

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,17 @@
-// @ts-nocheck
 import globals from 'globals';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     languageOptions: {
       globals: globals.browser
     },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "allowObjectTypes": "off",
+    }
   },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
 ];
