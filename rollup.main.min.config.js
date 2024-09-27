3ad1b88b770ee39c2c9a50c2161802da2bcd6d85
Esther Brunner <esther.brunner@zeix.com>
2024-09-27 15:18:00 +0200

    setup docs

--- rollup.main.min.config.js
+++ rollup.main.min.config.js
@@ -1,25 +1,34 @@
-import typescript from '@rollup/plugin-typescript';
-import terser from '@rollup/plugin-terser';
-import replace from '@rollup/plugin-replace';
-import dotenv from 'dotenv';
+import typescript from '@rollup/plugin-typescript'
+import terser from '@rollup/plugin-terser'
+import replace from '@rollup/plugin-replace'
+import copy from 'rollup-plugin-copy'
+import dotenv from 'dotenv'
 
-const env = dotenv.config({ path: `.env.${process.env.NODE_ENV}` }).parsed;
+const env = dotenv.config({ path: `.env.${process.env.NODE_ENV}` }).parsed
 
 export default {
-  input: 'index.ts',
-  output: {
-    dir: './',
-    format: 'esm',
-    entryFileNames: '[name].min.js',
-    chunkFileNames: 'dist/chunks/[name]-[hash].js',
-    inlineDynamicImports: true
-  },
-  plugins: [
-    typescript(),
-    terser(),
-    replace({
-      preventAssignment: true,
-      'process.env.DEV_MODE': JSON.stringify(env.DEV_MODE),
-    }),
-  ]
-};
+	input: 'index.ts',
+	output: {
+		dir: './',
+		format: 'esm',
+		entryFileNames: '[name].min.js',
+		chunkFileNames: 'dist/chunks/[name]-[hash].js',
+		inlineDynamicImports: true
+	},
+	plugins: [
+		typescript(),
+		terser(),
+		replace({
+		preventAssignment: true,
+			'process.env.DEV_MODE': JSON.stringify(env.DEV_MODE),
+		}),
+		copy({
+			targets: [
+				{ 
+					src: 'index.min.js',
+					dest: 'docs/assets/js/'
+				}
+			]
+		})
+	]
+}
